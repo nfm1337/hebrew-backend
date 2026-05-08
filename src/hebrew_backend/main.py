2b586fc0516640263ba0a9a418191fbe38c3ff39
Daniel Olovianniy <daniel.olovianniy@gmail.com>
2026-05-09 01:36:40 +0300

    feat: add base fastapi initialization

---
+++ src/hebrew_backend/main.py
@@ -0,0 +1,17 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from hebrew_backend.database import engine
+from hebrew_backend.routers import health
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(health.router)
